12a93bd545cbbfd6b8485f8be588ae3f5f85fb1b
Arpit Sharma <AS.73881@GMAIL.COM>
2024-07-27 07:02:15 +0530

    variables

---
+++ 04-Variables/script.js
@@ -0,0 +1,44 @@
+var firstName = "Arpit";
+let lastName = "Sharma";
+let userAge = 24;
+
+let isHappy = true;
+
+
+let a = undefined;
+let b;
+let c = null;
+
+userAge = 25;
+
+firstName = 100;
+
+
+const birthYear = 1999;
+
+{
+
+    var test01 = "var";
+    var test01 = 500;
+}
+
+{
+
+    let test02 = "let";
+    let test02 = 400;
+}
+
+{
+    const test03 = " const";
+    const test03 = 300;
+}
+
+let userIntro =
+    "Hi I am " + firstName + " " + lastName + ". " + "My age is " + userAge;
+
+// unable to change the const value
+// birthYear = 2000;
+
+let पर_मान = "name";
+let परमान = "name";
+let परमान12 = "name";
