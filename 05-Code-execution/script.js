14e5d3aafcbf1bed929e33fd23d19e821ea2b81c
Arpit Sharma <AS.73881@GMAIL.COM>
2024-07-29 07:24:20 +0530

    code execution

---
+++ 05-Code-execution/script.js
@@ -0,0 +1,9 @@
+// debugger;
+console.log(firstName);
+
+var firstName = "Arpit";
+let lastName = "Sharma";
+let age = 15;
+const yearOfBirth = 1999;
+
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
