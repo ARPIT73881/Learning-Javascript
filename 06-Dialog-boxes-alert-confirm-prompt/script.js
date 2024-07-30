8218e30441620745244d7ee1682380a660a968a3
Arpit Sharma <AS.73881@GMAIL.COM>
2024-07-30 11:08:36 +0530

    Dialog box alert confirm prompt

---
+++ 06-Dialog-boxes-alert-confirm-prompt/script.js
@@ -0,0 +1,6 @@
+// alert("Your message goes here.");
+// confirm("Your message goes here.");
+// const isConfirmed = confirm("Would you like to proceed ?");
+// console.log(isConfirmed);
+const confirmed = prompt("Your message goes here.");
+console.log(confirmed);
