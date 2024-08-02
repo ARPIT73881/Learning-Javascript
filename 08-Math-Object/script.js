5ac606386da6038718283ab24a6012f70b678375
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-02 09:09:07 +0530

    math object

---
+++ 08-Math-Object/script.js
@@ -0,0 +1,10 @@
+const length = +prompt("Enter length");
+const breadth = +prompt("Enter breadth");
+const area = length * breadth;
+// alert(`Area of rectangle is ${area} .`);
+
+// console.log(length);
+// console.log(breadth);
+// console.log(area);
+
+document.write(`Area = ${area}`);
