243a803b2186cc073f173ac3a11375dc1bb2aa85
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-07 21:27:44 +0530

    ternary operator

---
+++ 15-Ternary-Operator/script.js
@@ -0,0 +1,8 @@
+// debugger;
+// const userName = 5 > 3 ? "Arpit" : "Procoder";
+const gender = prompt("Select your gender");
+
+console.log(
+  `${gender.toLowerCase() == "f" ? "She" : "He"} is a college student`
+);
+// console.log(userName);
