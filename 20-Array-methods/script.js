d94b7cde69b6f73b0b6066a4e8672e2d62e4cac7
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-22 09:29:07 +0530

    array methods

---
+++ 20-Array-methods/script.js
@@ -0,0 +1,55 @@
+// const evenNumbers = [0, 2, 4, 6, 8];
+// const oddNumbers = [1, 3, 5, 7, 9];
+// const animals = ["lion", "tiger"];
+
+// const newArray1 = animals.concat(evenNumbers);
+// const newArray2 = evenNumbers.concat(animals);
+// const newArray3 = animals.concat(oddNumbers, evenNumbers);
+// pop() --> remove element from last
+// evenNumbers.pop();
+
+//push() --> add element to last
+// evenNumbers.push(8);
+
+//shift() --> remove element from starting
+// evenNumbers.shift();
+
+//unshift()  --> add element to start
+// evenNumbers.unshift(0);
+
+//concat()  --> adding arrays
+// const newArray = animals.concat(evenNumbers);
+
+//indexOf()  --> return the index
+// evenNumbers.indexOf(4);
+// animals.indexOf("Lion");
+
+//includes()  --> check for the element is present or not
+// animals.includes("cat")
+
+//reverse()  --> reverse the original array
+// evenNumbers.reverse();
+
+//sort()
+// nonLivingItems.sort()
+// const nonLivingItems = ["table", "chair", "fan", "stool", "bed"];
+
+// const animals = ["dog", "cat", "rat", "lion", "elephant", "cow", "tiger"];
+// slice() --> use to cut items in the array
+// const sliceArray = animals.slice(1, 4);
+// console.log(animals);
+// console.log(sliceArray);
+
+// const animals = ["dog", "cat", "rat", "lion", "elephant", "cow", "tiger"];
+// animals.splice(2, 1);
+// console.log(animals);
+// animals.splice(3, 2);
+// console.log(animals);
+//splice --> delete elements from original array
+// removes elements by indexing and also counts upto which element you want to remove
+// it also can add elements and modify original elements
+
+// const animals2 = ["dog", "cat", "rat", "lion", "elephant", "cow", "tiger"];
+// console.log(animals2);
+// animals2.splice(2, 2, "Arpit", "007", 3, null);
+// console.log(animals2);
