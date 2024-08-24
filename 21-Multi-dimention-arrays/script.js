e583fb3c027ea30c8fab583f73d3ad6be1690e59
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-24 08:11:34 +0530

    multi dimention array

---
+++ 21-Multi-dimention-arrays/script.js
@@ -0,0 +1,12 @@
+// const array = [7, "arpit"];
+const studentMarks = [
+  ["Arpit", 90],
+  ["Anurag", 88],
+  ["Ashutosh", 77],
+];
+
+const ticTacToe = [
+  ["x", null, null],
+  [null, null, "o"],
+  ["x", null, "o"],
+];
