2ff6364125031d03d13db2e6815e07ce576afcf1
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-25 07:16:06 +0530

    compound operator

---
+++ 23-Compound-operator/script.js
@@ -0,0 +1,15 @@
+let num = 2;
+// num = num + 2;
+// num = num + 2;
+// num += 2;
+
+// const newNum = num++;
+// const newNum = ++num;
+
+// similar to
+// num += 1  <==> num++
+
+const number = 3;
+// error --> number = number + 1;
+//error --> number += 1;
+//error --> number++;
