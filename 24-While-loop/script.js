4079bc7e36a6c0b613ae29392f97948406e93f13
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-25 07:16:25 +0530

    while loop

---
+++ 24-While-loop/script.js
@@ -0,0 +1,23 @@
+console.log("Program Started");
+
+// let num = 0;
+
+// while (num < 10) {
+//   console.log(num);
+//   num = num + 1;
+// }
+
+const array = ["aaa", "bbb", "ccc", "ddd", "eee"];
+//normally
+// console.log(array[0]);
+// console.log(array[1]);
+
+//loop
+let i = 0;
+while (i < array.length) {
+  //   console.log(array[i]);
+  console.log(`${i}. ${array[i]}  procoder`);
+  i++;
+}
+
+console.log("Program Ended");
