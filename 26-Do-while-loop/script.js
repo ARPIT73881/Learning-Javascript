3682eb8ab409447a3ebca83d21c9fb329006b272
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-28 07:16:10 +0530

    do while loop

---
+++ 26-Do-while-loop/script.js
@@ -0,0 +1,23 @@
+console.log("Program Start");
+
+// while
+// let i = 0
+// while (i < 5) {
+//   console.log(i)
+//   i++
+// }
+
+// for
+// for(let i = 0; i < 5; i++) {
+//     console.log(i);
+// }
+
+let i = 0;
+
+// do while
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
+
+console.log("Program Ended");
