07fbe49b76e5ad27f13071ed940db5e6611cb8fd
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-30 07:45:10 +0530

    execution context

---
+++ 29-Execution-context/script.js
@@ -0,0 +1,26 @@
+// debugger;
+// console.log(firstName);
+
+// var firstName = "Akash";
+// let lastName = "Singh";
+// let age = 15;
+// const yearOfBirth = 1999;
+
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
+
+debugger;
+sayHi();
+const username = "Arpit";
+const userAge = 25;
+// console.log(username);
+function sayHi() {
+  const a = 14;
+  const b = 12;
+  add(10, 20);
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log("Program ended");
