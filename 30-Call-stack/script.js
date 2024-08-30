13cfce34363887ce8cafcf30fb390d80586a9c7b
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-30 07:45:32 +0530

    call stack

---
+++ 30-Call-stack/script.js
@@ -0,0 +1,31 @@
+// debugger;
+
+// sayHi();
+
+// const username = "Anurag";
+// const userAge = 25;
+
+// function sayHi() {
+//   const a = 14;
+//   const b = 12;
+//   add(7, 9);
+// }
+
+// function add(x, y) {
+//   kuchhBhi();
+//   return x + y;
+// }
+
+// function kuchhBhi() {
+//   console.log("Kuchh bhi");
+// }
+
+// debugger;
+function intro() {
+  console.log("Hi , my name is Arpit");
+  intro();
+}
+
+intro();
+
+console.log("Program Ended");
