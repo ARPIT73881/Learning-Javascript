02bbdbc1971acf4ac7959a79d8d0dc4b00719431
Arpit Sharma <AS.73881@GMAIL.COM>
2024-08-31 06:26:01 +0530

    Global scope vs local scope

---
+++ 32-Global-scope-vs-local-scope/script.js
@@ -0,0 +1,21 @@
+// global variables
+const username = "Arpit";
+const userAge = 25;
+
+function add() {
+  // local variables
+  const x = 5;
+  const y = 8;
+  console.log(x + y);
+}
+function sub() {
+  // local variables
+  const u = 10;
+  const v = 2;
+  console.log(u - v);
+}
+
+add();
+sub();
+
+console.log("Program ended");
