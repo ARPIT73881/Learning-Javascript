48e298e8e11751cb805ffe13612b4bdf67305143
Arpit Sharma <AS.73881@GMAIL.COM>
2024-09-05 06:21:36 +0530

    methods and functions

---
+++ 39-Methods-and-functions/script.js
@@ -0,0 +1,18 @@
+const maths = {
+  E: 2.718281828459045,
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+  square: function (a) {
+    return a * a;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+  cube(a) {
+    return a ** 3;
+  },
+};
